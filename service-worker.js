e103fd467db6a4eca7b8e707a987f302574e635c
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-12 14:54:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.45c5918103d5fca983df0dc68a40633b.js"
 );
 
 self.addEventListener('message', (event) => {
